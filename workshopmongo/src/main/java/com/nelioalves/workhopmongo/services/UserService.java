9cb4b73634a34e9a6b50be5ebacbc9a04785aa8d
LuizAndrielli <r4t0X@hotmail.com>
2022-08-31 21:54:15 -0300

    Obtendo um usuário por id

--- workshopmongo/src/main/java/com/nelioalves/workhopmongo/services/UserService.java
+++ workshopmongo/src/main/java/com/nelioalves/workhopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.nelioalves.workhopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nelioalves.workhopmongo.domain.User;
 import com.nelioalves.workhopmongo.repository.UserRepository;
+import com.nelioalves.workhopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,4 +19,9 @@ public class UserService {
 	public List<User> findAll(){
 		return repo.findAll();
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
 }
