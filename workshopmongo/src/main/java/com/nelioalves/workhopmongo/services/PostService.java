aee80149b942b75ab9d5a7c8d0b4a379eaf34498
LuizAndrielli <r4t0X@hotmail.com>
2022-09-04 00:33:33 -0300

    Consulta simples com @Query

--- workshopmongo/src/main/java/com/nelioalves/workhopmongo/services/PostService.java
+++ workshopmongo/src/main/java/com/nelioalves/workhopmongo/services/PostService.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nelioalves.workhopmongo.domain.Post;
-import com.nelioalves.workhopmongo.domain.User;
-import com.nelioalves.workhopmongo.dto.UserDTO;
 import com.nelioalves.workhopmongo.repository.PostRepository;
-import com.nelioalves.workhopmongo.repository.UserRepository;
 import com.nelioalves.workhopmongo.services.exception.ObjectNotFoundException;
 
 @Service
@@ -23,4 +20,8 @@ public class PostService {
 		Optional<Post> user = repo.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Post não encontrado"));
 	}	
+
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
 }
